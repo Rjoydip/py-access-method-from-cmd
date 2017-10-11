fcace162c134739c5e7e41155d1f21a0b40b52f2
Rjoydip <joydipand@gmail.com>
2017-10-11 00:08:04 -0800

    nos_args moved 1 line above

--- decorator.py
+++ decorator.py
@@ -8,13 +8,15 @@ def execute(fn):
             method, params = sys.argv[1], sys.argv[2:]
             module_name = sys.modules[fn.__module__]
             methods = dir(module_name)
+            nos_arg = getattr(module_name, methods[methods.index(method)]).__code__.co_argcount
+
             if method in methods:
-                nos_arg = getattr(module_name, methods[methods.index(method)]).__code__.co_argcount
                 if nos_arg == 0 and len(params) == 0:
                     getattr(module_name, methods[methods.index(method)])()
                 elif nos_arg == len(params):
                     getattr(module_name, methods[methods.index(method)])(params)
                 else:
                     print("Paramater miss-match")
+            #endif        
             return fn()
     return wrapper
